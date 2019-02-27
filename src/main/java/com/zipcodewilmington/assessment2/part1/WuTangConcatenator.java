9e292a3689f54437d240c9b32a182e5220262c22
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-02-27 13:42:26 -0500

    Completed WutangConcatenator

--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -1,18 +1,27 @@
 package com.zipcodewilmington.assessment2.part1;
 
 public class WuTangConcatenator {
+
+    private Integer input;
     public WuTangConcatenator(Integer input) {
+        this.input = input;
     }
 
     public Boolean isWu() {
-        return null;
+        Boolean result = false;
+        if(input % 3 == 0){ result = true; }
+        return result;
     }
 
     public Boolean isTang() {
-        return null;
+        Boolean result = false;
+        if(input % 5 == 0){ result = true; }
+        return result;
     }
 
     public Boolean isWuTang() {
-        return null;
+        Boolean result = false;
+        if(isWu() && isTang()){ result = true; }
+        return result;
     }
 }
