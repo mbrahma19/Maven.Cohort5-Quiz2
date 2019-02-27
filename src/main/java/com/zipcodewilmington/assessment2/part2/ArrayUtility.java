5b2e8034f90c1b6c51a45924489f7745890ee08e
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-02-27 14:47:24 -0500

    Completed part 2.1

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,19 +1,52 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        ArrayList<Integer> array1AsList = returnAnIntegerList(array1);
+        ArrayList<Integer> array2AsList = returnAnIntegerList(array2);
+
+        array1AsList.addAll(array2AsList);
+        return returnIntegeArray(array1AsList);
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
-        return null;
+        ArrayList<Integer> rotatedArrayList = new ArrayList<>();
+
+        for(int i = 0 ; i < array.length; i++){
+            //TODO: complete the method at the end
+        }
+
+        return returnIntegeArray(rotatedArrayList);
     }
 
     public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
-        return null;
+        ArrayList<Integer> arrayList = returnAnIntegerList(merge(array1,array2));
+        return Collections.frequency(arrayList,valueToEvaluate);
     }
 
     public Integer mostCommon(Integer[] array) {
-        return null;
+        ArrayList<Integer> arrayList = returnAnIntegerList(array);
+        Integer mostOccurences = arrayList.get(0);
+
+        for(Integer i : arrayList){
+            if(Collections.frequency(arrayList,i) > Collections.frequency(arrayList,mostOccurences)){
+                mostOccurences = arrayList.get(i);
+            }
+        }
+        return mostOccurences;
     }
+
+    public ArrayList<Integer> returnAnIntegerList(Integer[] arrayToMorph){
+        return new ArrayList<>(Arrays.asList(arrayToMorph));
+    }
+
+    public Integer[] returnIntegeArray(ArrayList<Integer> arrayList){
+        return arrayList.toArray(new Integer[arrayList.size()]);
+    }
+
 }
