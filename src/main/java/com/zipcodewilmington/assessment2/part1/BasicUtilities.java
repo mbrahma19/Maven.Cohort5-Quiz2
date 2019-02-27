0b2c1d2be1d48205a106e2317d13e9803401e7f0
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-02-27 13:52:05 -0500

    Complated basic utilities

--- src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
@@ -2,18 +2,22 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class BasicUtilities {
     public Boolean isGreaterThan5(Integer value) {
-        return null;
+        return value >= 5;
     }
 
     public Boolean isLessThan7(Integer value) {
-        return null;
+        return value <= 7;
     }
 
     public Boolean isBetween5And7(Integer valueToEvaluate) {
-        return null;
+        Boolean isGreaterThan5 = isGreaterThan5(valueToEvaluate);
+        Boolean isLessThan7 = isLessThan7(valueToEvaluate);
+
+        return isGreaterThan5.equals(isLessThan7);
     }
 
     public Boolean startsWith(String string, Character character) {
-        return null;
+        String charToCompare = string.toLowerCase().substring(0,1);
+        return character.toString().toLowerCase().equals(charToCompare);
     }
 }
